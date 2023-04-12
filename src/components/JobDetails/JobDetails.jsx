e2a11456ee49c18c57b552a53a79746ef9e40574
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-04-12 21:04:18 +0600

    details page half done

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import './JobDetails.css'
 
 const JobDetails = () => {
   const { id } = useParams();
@@ -9,8 +10,12 @@ const JobDetails = () => {
     <div>
         <h2>Job Details</h2>
         <div>
-            <p>{jobs.job_description}</p>
-            <p>{jobs.job_responsibility}</p>
+            <p><span className='bold'>Job Description: </span>{jobs.job_description}</p>
+            <p><span className='bold'>Job Responsibility: </span>{jobs.job_responsibility}</p>
+            <p className='bold'>Educational Requirements:</p>
+            <p>Bachelor degree to complete any reputational university</p>
+            <p className='bold'>Experiences:</p>
+            <p>2-3 years in this field</p>
         </div>
     </div>
   );
