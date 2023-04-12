69d457b874af14206ccb85e7401b65f49ba48b5e
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-04-12 21:47:38 +0600

    details page done

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -8,15 +8,34 @@ const JobDetails = () => {
   console.log(jobs);
   return (
     <div>
-        <h2>Job Details</h2>
-        <div>
-            <p><span className='bold'>Job Description: </span>{jobs.job_description}</p>
-            <p><span className='bold'>Job Responsibility: </span>{jobs.job_responsibility}</p>
-            <p className='bold'>Educational Requirements:</p>
-            <p>Bachelor degree to complete any reputational university</p>
-            <p className='bold'>Experiences:</p>
-            <p>2-3 years in this field</p>
+      <h2>Job Details</h2>
+      <div className='details-container'>
+        <div className='details-right'>
+          <p><span className='bold'>Job Description: </span>{jobs.job_description}</p>
+          <p><span className='bold'>Job Responsibility: </span>{jobs.job_responsibility}</p>
+          <p className='bold'>Educational Requirements:</p>
+          <p>{jobs.requirements}</p>
+          <p className='bold'>Experiences:</p>
+          <p>{jobs.experiences}</p>
         </div>
+        <div className='details-left'>
+          <div className='left'>
+            <h3>Job Details</h3>
+            <hr />
+            <p><span className='bold'>Salary: </span>{jobs.salary}</p>
+            <p><span className='bold'>Job Title: </span>Product Designer</p>
+            <h3>Contact Information</h3>
+            <hr />
+            <p><span className='bold'>Phone:</span>01720- 00 00 00</p>
+            <p><span className='bold'>Email: </span>{jobs.contact_info}</p>
+            <p>Dhaka, Bangladesh</p>
+          </div>
+          <div>
+            <button className='btn btn-d'>Apply Now</button>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
